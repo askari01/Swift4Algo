969766f0c13dbd33ddc674d0d8e9689e30738ae0
Jacopo <jmangia@me.com>
2017-10-25 15:41:05 -0700

    clean up

--- fibonacci/fibonacci.swift
+++ fibonacci/fibonacci.swift
@@ -15,10 +15,10 @@ fibRecursive(8)
 time
 
 //n (n space + stack)
-class FibDynamic {
+func fibDynamic(_ n: Int) -> Int {
     var mem:[Int:Int] = [:]
     
-    func fibDynamic(_ n: Int) -> Int {
+    func _fibDynamic(_ n: Int) -> Int {
         time += 1
         guard n > 1 else { return n }
         
@@ -26,34 +26,33 @@ class FibDynamic {
             return memo
         }
         
-        let result = fibDynamic(n - 1) + fibDynamic(n - 2)
+        let result = _fibDynamic(n - 1) + _fibDynamic(n - 2)
         mem[n] = result
         
         return result
     }
+    
+    return _fibDynamic(n)
 }
 
 time = 0
-FibDynamic().fibDynamic(8)
+fibDynamic(8)
 time
 
 //n (1 space)
 func fibReverseLinear(_ n: Int) -> Int {
     guard n > 1 else { return n }
     
-    var prevPrev = 0
-    var prev = 1
-    var current = 0
-    
-    for _ in 1..<n {
-        current = prev + prevPrev
-        prevPrev = prev
-        prev = current
+    var this = 1
+    var previous = 0
+    for _ in 2...n {
+        (this, previous) = (this + previous, this)
     }
     
-    return current
+    return this
 }
 
-fibReverseLinear(8)
 
-
+for i in 0..<16 {
+    print("\(i) = \(fibRecursive(i)) - \(fibReverseLinear(i))")
+}
