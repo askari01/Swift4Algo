65eeac0f5b8a8033d77496a3b92359c956911787
J-Mangia <j.mangia@ibm.com>
2018-02-15 12:33:20 -0800

    fix

--- triplette/triplette.swift
+++ triplette/triplette.swift
@@ -12,19 +12,20 @@ func findTriplette(_ a: [Int]) -> [[Int]] {
     var i=0, j=1, l=2, o=0
     
     func n() { i=o+1; j=i+1; l=i+2; o=i }
+    func n2() { if j<c-2 { j+=1; l=j+1 } else { n() } }
     
     while i<c && j<c && l<c {
         let t = s[i]+s[j]+s[l]
+        
         if t == 0 {
             r.append([s[i], s[j], s[l]])
-            n()
+            n2()
         }
         else if t<0 {
             if l<c-1 { l+=1 }
-            else if j<c-1 { j+=1; l=j+1 }
-            else { n() }
+            else { n2() }
         }
-        else {break}
+        else { n2() }
     }
     
     return r
@@ -71,6 +72,6 @@ func findTripletteCubic(_ a: [Int]) -> [[Int]] {
     return r.map { $0.array }
 }
 
-print(findTriplette([5, -3, 0, 4, -2, 1, -1, 2, 3]))
 print(findTripletteCubic([5, -3, 0, 4, -2, 1, -1, 2, 3]))
+print(findTriplette([5, -3, 0, 4, -2, 1, -1, 2, 3]))
 
